9faf53f0712e8f6c0e00406e051f8bdd6bbabab9
Daniele Tentoni <d.tentoni@wedoit.io>
2021-08-10 15:36:30 +0200

    Add new routes to main application.

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/forecasts/:locality",
+    name: "Forecasts",
+    component: Home,
+  },
+  {
+    path: "/feedbacks",
+    name: "Feedbacks",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "*",
+    name: "Error 404",
+    component: Home, // TODO: Generate a proper 404 page.
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
