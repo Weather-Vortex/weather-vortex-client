e70deef9f732a289cd4767f47bb491d933546d59
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-08-15 11:17:16 +0200

    registration from finished

--- src/views/users/Register.vue
+++ src/views/users/Register.vue
@@ -1,154 +1,152 @@
 <template>
-  <div class="login">
-    <v-app>
-      <v-main>
-        <v-container class="fill-height" fluid>
-          <v-row align="center" justify="center" dense>
-            <v-col cols="12" sm="8" md="4" lg="4">
-              <v-card elevation="0">
-                <v-img
-                  src="@/assets/logo.png"
-                  alt="Weather Vortex logo"
-                  contain
-                  height="200"
-                ></v-img>
+  <v-container fluid class="fluid">
+    <v-row justify="center" align="center" class="row">
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+        xs="4"
+        class="text-center"
+        style="height: 100vh"
+      >
+        <h1 justify="center">Welcome to</h1>
+        <v-img
+          src="@/assets/vortex.png"
+          alt="Weather Vortex logo"
+          contain
+          height="200"
+        ></v-img>
+        <p>Please complete this form to create an account</p>
 
-                <v-card-text>
-                  <v-form ref="form" class="mx-2" v-model="valid" lazy-validation >
-                    <v-row>
-                      <v-col cols="12" sm="6">
-                        <v-text-field
-                          label="Enter your name"
-                          name="name"
-                          prepend-inner-icon="mdi-account"
-                          type="text"
-                          class="rounded-0"
-                          ref="name"
-                          v-model="name"
-                          :rules="[rules.required]"
-                          :error-messages="errorMessages"
-                          required
-                          placeholder="FirstName"
-                          outlined
-                        >
-                          ></v-text-field
-                        >
-                      </v-col>
-
-                      <v-col cols="12" sm="6">
-                        <v-text-field
-                          label="Enter your surname"
-                          name="surname"
-                          prepend-inner-icon="mdi-account"
-                          type="text"
-                          class="rounded-0"
-                          ref="surname"
-                          v-model="surname"
-                          :rules="[rules.required]"
-                          :error-messages="errorMessages"
-                          placeholder="Surname"
-                          required
-                          outlined
-                        >
-                        </v-text-field>
-                      </v-col>
-                    </v-row>
-
-                    <v-text-field
-                      label="Enter your email"
-                      name="email"
-                      prepend-inner-icon="mdi-email"
-                      type="email"
-                      class="rounded-0"
-                      v-model="email"
-                      :rules="[rules.required]"
-                      :error-messages="errorMessages"
-                      placeholder="Email"
-                      required
-                      outlined
-                    ></v-text-field>
-                    <v-text-field
-                      v-model="password"
-                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :rules="[rules.required, rules.min, rules.pwd]"
-                      :type="show1 ? 'text' : 'password'"
-                      label="Enter your password"
-                      name="password"
-                      hint="At least 8 characters"
-                      counter
-                      @click:append="show1 = !show1"
-                      prepend-inner-icon="mdi-lock"
-                      class="rounded-0"
-                      placeholder="Password"
-                      :error-messages="errorMessages"
-                      required
-                      outlined
-                    ></v-text-field>
-                    <v-btn
-                      class="rounded-0"
-                      color="#000000"
-                      x-large
-                      block
-                      dark
-                      @click="submit"
-                      >Signup</v-btn
-                    >
-
-                    <v-card-actions class="text--secondary">
-                      <v-spacer></v-spacer>
-                      <!-- <router-link :to="{ name: 'SignUp' }">Sign Up</router-link> -->
-                      Already have an account?
-                      <a href="#" class="pl-2" style="color: #000000"
-                        >Sign In</a
-                      >
-                    </v-card-actions>
-                  </v-form>
-                </v-card-text>
-                <v-card-actions class="ml-6 mr-6 text-center">
-                  <p>
-                    By continuing, you agree to Weather Vortex's
-                    <a href="#" class="pl-2" style="color: #000000">Policy</a>
-                    and
-                    <a href="#" class="pl-2" style="color: #000000"
-                      >Terms of Use</a
-                    >
-                  </p>
-                </v-card-actions>
-              </v-card>
+        <v-form ref="form" class="mx-2" v-model="valid" lazy-validation>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="First Name"
+                required
+                prepend-inner-icon="mdi-account"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                label="Last Name"
+                prepend-inner-icon="mdi-account"
+                required
+              ></v-text-field>
             </v-col>
           </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </div>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="Email"
+                prepend-inner-icon="mdi-email"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="password"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show1 ? 'text' : 'password'"
+                @click:append="show1 = !show1"
+                :rules="passwordRules"
+                label="Password"
+                prepend-inner-icon="mdi-lock"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="retypepassword"
+                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show2 ? 'text' : 'password'"
+                @click:append="show2 = !show2"
+                :rules="passwordRules"
+                label="Re-type Password"
+                prepend-inner-icon="mdi-lock"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-checkbox
+            v-model="firstcheckbox"
+            :rules="[(v) => !!v || 'You must agree to continue!']"
+            label="I agree with Terms and Conditions"
+            required
+          ></v-checkbox>
+
+          <v-checkbox
+            v-model="seccheckbox"
+            label="I want to receive WeatherVortex Emails"
+            required
+          ></v-checkbox>
+
+          <v-btn
+            class="rounded-0"
+            color="#000000"
+            x-large
+            block
+            dark
+            @click="submitForm"
+          >
+            Register
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style lang="css" scoped>
-</style>
 <script>
 export default {
-  //la località preferita secondo me ci sta nell'account
+  name: "RegisterForm",
   data: () => ({
     show1: false,
-    password: null,
-    name: null,
-    surname: null,
-    email: null,
-
-    rules: {
-      required: (value) => !!value || "This field is required",
-      min: (v) => v.length >= 8 || "Min 8 characters",
-      pwd: (v) =>
+    show2: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) =>
+        /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        ) || "E-mail must be valid",
+    ],
+    password: "",
+    retypepassword: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) =>
         /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(v) ||
         "Password must contain at least lowercase letter, one number, a special character and one uppercase letter",
-      //emailMatch: () => `The email and password you entered don't match`,
-    },
-
-    methods: {
-      submit() {
-        this.$refs.form.validate();
-      },
-    },
+    ],
+    firstcheckbox: false,
+    seccheckbox: false,
   }),
+  methods: {
+    submitForm() {
+      this.$refs.form.validate();
+    },
+  },
 };
 </script>
+<style scoped>
+.fluid {
+  margin: 0;
+  padding: 0;
+}
+</style>
