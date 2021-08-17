19a5fa819b8f7ca3ab0c56334e27697d0f963a02
igor-lirussi <igor.lirussi@studio.unibo.it>
2021-08-17 11:39:20 +0200

    404 page centered

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,25 +1,22 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row :justify="center">
-      <v-col :justify="center" :align-self="center">
-        <v-card>
-          <v-img
-            src="../assets/mansky.jpg"
-            contain
-            max-height="283"
-            max-width="500"
-          >
-          </v-img>
-        </v-card>
+  <v-container fluid>
+    <v-row :align="center" :justify="center">
+      <v-spacer> </v-spacer>
+      <v-col cols="12" sm="8" md="4" xs="4">
+        <v-img
+          src="../assets/mansky.jpg"
+          contain
+          max-height="283"
+          max-width="500"
+        >
+        </v-img>
       </v-col>
+      <v-spacer> </v-spacer>
     </v-row>
     <v-row :justify="center">
       <v-col>
         <h1>This is 404 Error</h1>
       </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col>tast</v-col>
     </v-row>
   </v-container>
 </template>
