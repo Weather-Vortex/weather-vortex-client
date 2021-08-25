cf4c6d2c640f977d24bdb04e1fb83132a255da25
igor-lirussi <igor.lirussi@studio.unibo.it>
2021-08-25 19:45:35 +0200

    better 404 page

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,22 +1,42 @@
 <template>
   <v-container fluid>
     <v-row :align="center" :justify="center">
-      <v-spacer> </v-spacer>
-      <v-col cols="12" sm="8" md="4" xs="4">
-        <v-img
-          src="../assets/mansky.jpg"
-          contain
-          max-height="283"
-          max-width="500"
-        >
+      <v-spacer class="d-none d-md-block"></v-spacer>
+      <v-col cols="12" sm="12" md="6" xs="6">
+        <v-img src="../assets/404.png" contain max-height="483" max-width="750">
         </v-img>
       </v-col>
-      <v-spacer> </v-spacer>
+      <v-spacer class="d-none d-md-block"></v-spacer>
     </v-row>
     <v-row :justify="center">
       <v-col>
         <h1>This is 404 Error</h1>
+        This page is missing, maybe it's disappeared after a storm, or probably
+        you are lost...
+        <br />
+        <br />
+        <v-btn
+          class="rounded-xl"
+          color="primary"
+          x-large
+          dark
+          @click="goHome()"
+        >
+          <b>Go back <br />to a safe place</b>
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: "Error404",
+  data: () => ({}),
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
