e80e2cd4312b0475e92e324dfedf3ed486e6aa3b
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-08-29 15:59:53 +0200

    risolto scorrimento orrizzontale pagina about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,88 +1,86 @@
 <template>
-  <div class="aboutPage">
-    <div class="about my-4">
-      <v-row justify="center">
-        <v-spacer class="d-none d-md-block"></v-spacer>
-        <v-sheet class="mx-md-4 pa-4" color="white" elevation="4">
-          <h1>About</h1>
-          Weather Vortex is a project that aims to provide the user a unique
-          service to check all the weather forecasts and paragonate them.<br />
-          Starting from this need, this platform was created. Here it's possible
-          to semplify everyday decisions that are weather-based- <br />
-          This can lead to a huge amount of time saved and to a better
-          organization <br />
-          On the other hand, the user can also customize his experience adding
-          Iot device to "trust" and leave feedbacks to all the services
-          available.
-        </v-sheet>
-        <v-spacer class="d-none d-md-block"></v-spacer>
-      </v-row>
-    </div>
+  <v-container fluid class="aboutPage">
+    <v-row justify="center" align="center" class="row">
+      <v-spacer class="d-none d-md-block"></v-spacer>
+      <v-sheet class="mx-md-4 pa-4" color="white" elevation="4">
+        <h1>About</h1>
+        Weather Vortex is a project that aims to provide the user a unique
+        service to check all the weather forecasts and paragonate them.<br />
+        Starting from this need, this platform was created. Here it's possible
+        to semplify everyday decisions that are weather-based- <br />
+        This can lead to a huge amount of time saved and to a better
+        organization <br />
+        On the other hand, the user can also customize his experience adding Iot
+        device to "trust" and leave feedbacks to all the services available.
+      </v-sheet>
+      <v-spacer class="d-none d-md-block"></v-spacer>
 
-    <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
-      <v-row align="center" justify="center">
-        <v-col class="text-center " cols="12">
-          <h1
-            class="text-h4 font-weight-thin mb-4"
-            style="text-shadow: 1px 1px 2px black;"
-          >
-            Weather Vortex <br />Comparison
-          </h1>
-          <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
-            Take the right decision, today!
-          </h4>
-        </v-col>
-      </v-row>
-    </v-parallax>
+      <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center " cols="12">
+            <h1
+              class="text-h4 font-weight-thin mb-4"
+              style="text-shadow: 1px 1px 2px black;"
+            >
+              Weather Vortex <br />Comparison
+            </h1>
+            <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
+              Take the right decision, today!
+            </h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
 
-    <div class="timeline">
-      <h1>Timeline</h1>
-      <Timeline />
-    </div>
+      <v-container class="timeline">
+        <h1>Timeline</h1>
+        <Timeline />
+      </v-container>
 
-    <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
-      <v-row align="center" justify="center">
-        <v-col class="text-center " cols="12">
-          <h1
-            class="text-h4 font-weight-thin mb-4"
-            style="text-shadow: 1px 1px 2px black;"
-          >
-            Weather Vortex <br />Feedbacks
-          </h1>
-          <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
-            Choose who to trust!
-          </h4>
-        </v-col>
-      </v-row>
-    </v-parallax>
+      <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center " cols="12">
+            <h1
+              class="text-h4 font-weight-thin mb-4"
+              style="text-shadow: 1px 1px 2px black;"
+            >
+              Weather Vortex <br />Feedbacks
+            </h1>
+            <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
+              Choose who to trust!
+            </h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
 
-    <div class="meet my-3">
-      <h1>Meet the Team</h1>
-      <MeetTeam />
-    </div>
+      <v-container class="meet my-3">
+        <h1>Meet the Team</h1>
+        <MeetTeam />
+      </v-container>
 
-    <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
-      <v-row align="center" justify="center">
-        <v-col class="text-center " cols="12">
-          <h1
-            class="text-h4 font-weight-thin mb-4"
-            style="text-shadow: 1px 1px 2px black;"
-          >
-            Weather Vortex <br />Iot
-          </h1>
-          <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
-            Full customizable experience! <br />
-            Add your Iot stations, choose your favourite position, get notified!
-          </h4>
-        </v-col>
-      </v-row>
-    </v-parallax>
+      <v-parallax class="my-3" height="300" dark src="../assets/clouds.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center " cols="12">
+            <h1
+              class="text-h4 font-weight-thin mb-4"
+              style="text-shadow: 1px 1px 2px black;"
+            >
+              Weather Vortex <br />Iot
+            </h1>
+            <h4 class="subheading" style="text-shadow: 1px 1px 2px black;">
+              Full customizable experience! <br />
+              Add your Iot stations, choose your favourite position, get
+              notified!
+            </h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
 
-    <div class="contact">
-      <h1>Contact Us</h1>
-      <ContactUs />
-    </div>
-  </div>
+      <v-container class="contact">
+        <h1>Contact Us</h1>
+        <ContactUs />
+      </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
