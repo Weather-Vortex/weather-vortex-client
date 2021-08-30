9fc7b6e056362d6202436019f98a27e69592e774
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-08-30 12:38:39 +0200

    controls in fields edit

--- src/components/user/EditDialog.vue
+++ src/components/user/EditDialog.vue
@@ -9,26 +9,38 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="text-h5">User Profile</span>
+          <span class="text-h5">Edit some info</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12">
-                <v-text-field label="Email"></v-text-field>
-              </v-col>
-
               <v-col cols="12" sm="6">
-                <v-text-field label="Password" type="password"></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="show1 ? 'text' : 'password'"
+                  @click:append="show1 = !show1"
+                  :rules="passwordRules"
+                  label="Password"
+                  prepend-inner-icon="mdi-lock"
+                ></v-text-field>
               </v-col>
               <v-col cols="12" sm="6">
                 <v-text-field
-                  label="Retype Password"
-                  type="password"
+                  v-model="retypepassword"
+                  :rules="passwordRules.concat(validatePassword2)"
+                  :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="show2 ? 'text' : 'password'"
+                  @click:append="show2 = !show2"
+                  label="Re-type Password"
+                  prepend-inner-icon="mdi-lock"
                 ></v-text-field>
               </v-col>
               <v-col cols="12">
-                <v-text-field label="Position Preferred"></v-text-field>
+                <v-text-field
+                  label="Position Preferred"
+                  prepend-inner-icon="mdi-map-marker"
+                ></v-text-field>
               </v-col>
             </v-row>
           </v-container>
@@ -49,7 +61,21 @@
 <script>
 export default {
   data: () => ({
+    show1: false,
+    show2: false,
     dialog: false,
+    password: "",
+    retypepassword: "",
+    passwordRules: [
+      (v) =>
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(v) ||
+        "Password must contain at least lowercase letter, one number, a special character and one uppercase letter",
+    ],
   }),
+  methods: {
+    validatePassword2(value) {
+      return value === this.password || "Passwords don't match.";
+    },
+  },
 };
 </script>
