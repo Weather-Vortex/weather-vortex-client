cc758bd0c030856da71e99886367c6d8c9dd3a39
Daniele Tentoni <d.tentoni@wedoit.io>
2021-10-11 22:29:42 +0200

    Retrieve public info about users.

--- src/views/users/PublicProfile.vue
+++ src/views/users/PublicProfile.vue
@@ -1,49 +1,33 @@
 <template>
-  <v-app>
-    <v-main class="blue lighten-5">
-      <v-container align="center" justify="center">
-        <v-row class="bg-img" justify="space-around">
-          <v-col md="6" offset-md="3">
-            <PublicUserCard />
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <v-container
-        ><v-div></v-div>
-        <v-row class="mb-3" no-gutters>
-          <v-col md="4" class="pa-md-10 mx-lg-auto">
-            <h2 class="pa-md-8 sm-10 mx-lg-auto">Control Units</h2>
-
-            <PrivateUserControlUnits />
-          </v-col>
-          <v-col md="4" offset-md="4" class="pa-md-10 mx-lg-auto">
-            <h2 class="pa-md-8 sm-10 mx-lg-auto">Reviews</h2>
-            <PublicUserReviews />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-container align="center" justify="center">
+    {{ userId }}
+  </v-container>
 </template>
 
-<style>
-.bg-img {
-  /*l'immagine è anche in assets (clouds.jpg) ma non riesco a fargli riconoscere il percorso*/
-  background-image: url("clouds.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center top;
-  border-radius: 3px;
-}
-</style>
 <script>
-import PrivateUserControlUnits from "@/components/user/PrivateUserControlUnits";
-import PublicUserReviews from "@/components/user/PrivateUserReviews";
-import PublicUserCard from "@/components/user/PublicUserCard";
 export default {
-  name: "privateProfile",
-  components: { PrivateUserControlUnits, PublicUserReviews, PublicUserCard },
-  data: () => ({}),
+  name: "PublicProfile",
+  data: function () {
+    return {
+      userId: this.$route.params.id,
+    };
+  },
+  methods: {
+    loadUser() {
+      const server = process.env.VUE_APP_SERVER_URL;
+      const url = `${server}/users/${this.userId}`;
+      this.$http
+        .get(url)
+        .then((res) => {
+          console.log("Result:", res);
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    },
+  },
+  mounted() {
+    this.loadUser();
+  },
 };
 </script>
