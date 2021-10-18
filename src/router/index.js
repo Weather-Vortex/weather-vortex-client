40a97e4cef1dc3b404e505ad5a206ec9ce1c8567
Daniele Tentoni <d.tentoni@wedoit.io>
2021-10-18 20:09:54 +0200

    Resolve ESLINT.

--- src/router/index.js
+++ src/router/index.js
@@ -62,19 +62,23 @@ const routes = [
   {
     path: "/user",
     name: "User",
-    component: () => import("../views/User.vue"),
+    component: () => import(/* webpackChunkName: "user" */ "../views/User.vue"),
     children: [
       {
         path: "login",
         name: "Login",
-        component: () => import("../views/users/Login.vue"),
+        component: () =>
+          import(/* webpackChunkName: "login" */ "../views/users/Login.vue"),
         meta: {
           guest: true, //TODO da mettere una volta fato il logout
         },
       },
       {
         path: "register",
-        component: () => import("../views/users/Register.vue"),
+        component: () =>
+          import(
+            /* webpackChunkName: "register" */ "../views/users/Register.vue"
+          ),
         meta: {
           guest: true,
         },
@@ -82,38 +86,50 @@ const routes = [
       {
         name: "Personal Profile",
         path: "profile",
-        component: () => import("../views/users/Profile.vue"),
+        component: () =>
+          import(
+            /* webpackChunkName: "profile" */ "../views/users/Profile.vue"
+          ),
         meta: {
           requiresAuth: true,
         },
       },
       {
         path: "logout",
-        component: () => import("../views/users/Logout.vue"),
+        component: () =>
+          import(/* webpackChunkName: "logout" */ "../views/users/Logout.vue"),
         meta: {
           requiresAuth: true,
         },
       },
       {
         path: "public/:id",
-        component: () => import("../views/users/PublicProfile.vue"),
+        component: () =>
+          import(
+            /* webpackChunkName: "public_profile" */ "../views/users/PublicProfile.vue"
+          ),
       },
       {
         path: "confirm",
         /*props: route => ({ query: route.query }),*/
-        component: () => import("../views/users/Welcome.vue"),
+        component: () =>
+          import(
+            /* webpackChunkName: "welcome" */ "../views/users/Welcome.vue"
+          ),
         // props: true,
       },
       {
         path: "forgot",
-        component: () => import("../views/users/Forgot.vue"),
+        component: () =>
+          import(/* webpackChunkName: "forgot" */ "../views/users/Forgot.vue"),
       },
     ],
   },
   {
     path: "*",
     name: "Error 404",
-    component: () => import("../views/Error404.vue"),
+    component: () =>
+      import(/* webpackChunkName: "error404" */ "../views/Error404.vue"),
   },
 ];
 
