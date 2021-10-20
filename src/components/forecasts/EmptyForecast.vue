e072a50c5cf5c18d235e7a465b94e88c1eb8d14d
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-10-20 11:38:47 +0200

    img in forecast

--- src/components/forecasts/EmptyForecast.vue
+++ src/components/forecasts/EmptyForecast.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Search for a Forecast!</div>
+  <div>
+    Search for a Forecast!
+    <template>
+      <v-row align="center">
+        <v-col md="6" offset-md="3"
+          ><!--l'img è nella stessa directory di questa pagina--><v-img
+            align="center"
+            src="./weatherfewclouds.jpg"
+          ></v-img></v-col></v-row
+    ></template>
+  </div>
 </template>
 
 <script>
